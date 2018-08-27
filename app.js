d00473fa31c1ea10f0a94c1371794087030ce466
ciolt <cioltdev@outlook.com>
2018-08-27 01:20:27 -0400

    add robots.txt

--- app.js
+++ app.js
@@ -5,6 +5,10 @@ app.get('/', (req, res) => {
   res.send('This site is operated by Cyberscape. \nContact info@cyberscape.co for more information.')
 })
 
+app.get('/robots.txt', (req, res) => {
+  res.send('User-agent: *\nDisallow: ')
+})
+
 const listener = app.listen(process.env.PORT || 8080, () => {
   console.log('Site listening on port ' + listener.address().port)
 })

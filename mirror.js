c6bf0cd9581edaf52b2f930936cf93a7fe13274a
ciolt <5000732+ciolt@users.noreply.github.com>
2019-01-06 03:34:10 -0500

    Improve redirect analytics

--- mirror.js
+++ mirror.js
@@ -2,7 +2,7 @@ const app = require('express')()
 const PORT = process.env.PORT || 8080
 
 app.use((req, res) => res.header('X-Hello-From', 'Miami')
-  .redirect(301, 'https://cyberscape.co/')
+  .redirect(301, `https://cyberscape.co/?channel=cybernet&provider=${req.hostname || req.originalUrl || 'other'}`)
 )
 
 const listener = app.listen(PORT, () => {
